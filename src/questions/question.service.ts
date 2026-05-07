2fce0e7e810a60a5e3cc0ea63e1c813c3a852bc9
akashmore <moreakash029@gmail.com>
2026-05-07 14:44:09 +0530

    add organization-based filtering and access control for question retrieval and updates

--- src/questions/question.service.ts
+++ src/questions/question.service.ts
@@ -22,7 +22,7 @@ export class QuestionsService {
   }
 
   async getQuestions(req?: getQuestionsDTO) {
-    let match = {};
+    let match: any = {};
     if (req?.level) {
       match = { level: req.level };
     }
@@ -33,6 +33,13 @@ export class QuestionsService {
 
     if (req?.questionId !== null && req?.questionId !== undefined) {
       match = { ...match, _id: new mongooseTypes.ObjectId(req.questionId) };
+    }
+
+    if (req?.organizationId) {
+      match = {
+        ...match,
+        $or: [{ organizationId: req.organizationId }, { public: true }],
+      };
     }
     const count = await this.questionModel.find(match).count();
     const limit = req?.limit ? req.limit : count;
