8f89ede3da9a5056c592fb3ce68fc812117006ca
Kajal7004 <reshmikjl@gmail.com>
2026-05-11 16:56:12 +0530

    preview question wip

--- src/questions/question.controller.ts
+++ src/questions/question.controller.ts
@@ -249,6 +249,134 @@ export class QuestionsController {
       if (session) await session.endSession();
     }
   }
+    @Post('previewCustomQuestion')
+  @UsePipes(ValidationPipe)
+  async previewCustomQuestion(
+    @Req() request,
+    @Body() body: createCustomQuestionDTO,
+  ) {
+    try {
+      let orgId = request.payload['custom:orgId'];
+      const org = await this.authenticationService.getOrganisation({
+        _id: request.payload['custom:orgId'],
+      });
+      // if (org && org.subscriptionPlan === 'free') {
+      //   return {
+      //     message: 'no custom question for free plan',
+      //     statusCode: 402,
+      //   };
+      // }
+      let custQuestionCount = await this.questionsService.customQuestionCount({
+        organizationId: orgId,
+      });
+
+      // if (
+      //   custQuestionCount === org.availableTests &&
+      //   org.subscriptionPlan === 'paid'
+      // )
+      //   return {
+      //     message:
+      //       'You have used all Custom Questions, upgrade your plan to create more',
+      //     statusCode: 402,
+      //   };
+
+      //Check for each test case whether valid or not
+      let isValid;
+      [isValid, body] = checkTestCases(body);
+
+      if (!isValid) return body;
+
+      //Check for reserve Keyword in parameters
+
+      //Generate solution templates for each language.
+      //Create parameter string for each language to be inserted in solution template.
+      let cpp_solution_params = '';
+      let java_solution_params = '';
+      let python_javascript_solution_params = '';
+      let go_solution_params = '';
+      for (const param of body.inputType) {
+        cpp_solution_params =
+          cpp_solution_params +
+          getDatatypeOfParamters('cpp', param.type) +
+          ' ' +
+          param.paramName +
+          ',';
+        java_solution_params =
+          java_solution_params +
+          getDatatypeOfParamters('java', param.type) +
+          ' ' +
+          param.paramName +
+          ',';
+        python_javascript_solution_params =
+          python_javascript_solution_params + param.paramName + ',';
+
+        go_solution_params =
+          param.paramName +
+          ' ' +
+          getDatatypeOfParamters('go', param.type) +
+          ',';
+      }
+      //Remove comma from end of string
+      cpp_solution_params = cpp_solution_params.replace(/,$/g, '');
+      java_solution_params = java_solution_params.replace(/,$/g, '');
+      python_javascript_solution_params =
+        python_javascript_solution_params.replace(/,$/g, '');
+      go_solution_params = go_solution_params.replace(/.$/g, '');
+
+      //Replacing return type with outputType and parameters with generated params, inside the solution template.
+      console.log("🚀 ~ QuestionsController ~ previewCustomQuestion ~ body:", body)
+      body['solutionTemplates'] = [
+        {
+          language: 'cpp',
+          code: CPP_SOLUTION_TEMPLATE.replace(
+            'return_type',
+            getDatatypeOfParamters('cpp', body.outputType),
+          ).replace('parameters', cpp_solution_params),
+        },
+        {
+          language: 'java',
+          code: JAVA_SOLUTION_TEMPLATE.replace(
+            'return_type',
+            getDatatypeOfParamters('java', body.outputType),
+          ).replace('parameters', java_solution_params),
+        },
+        {
+          language: 'python',
+          code: PYTHON_SOLUTION_TEMPLATE.replace(
+            'parameters',
+            python_javascript_solution_params,
+          ),
+        },
+        {
+          language: 'javascript',
+          code: JAVASCRIPT_SOLUTION_TEMPLATE.replace(
+            'parameters',
+            python_javascript_solution_params,
+          ),
+        },
+        {
+          language: 'go',
+          code: GO_SOLUTION_TEMPLATE.replace(
+            'return_type',
+            getDatatypeOfParamters('go', body.outputType),
+          ).replace('parameters', go_solution_params),
+        },
+      ];
+
+      body['organizationId'] = new Types.ObjectId(
+        request.payload['custom:orgId'],
+      );
+      body['createdBy'] = request.payload.nickname;
+
+      return {
+        message: 'Question preview generated successfully',
+        statusCode: 200,
+        data: body,
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 
   @Patch('/updateCustomQuestion/:id')
   async update(@Param('id') id: string, @Body() body: any, @Req() request) {
